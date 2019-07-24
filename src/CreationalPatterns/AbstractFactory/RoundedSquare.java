7edb4817bdfb6357ae964bf1a6c52d59140d1111
Abdo-Sameh <abdo.sameh2000@gmail.com>
2019-07-24 11:52:58 +0200

    Abstract Factory design pattern

---
+++ src/CreationalPatterns/AbstractFactory/RoundedSquare.java
@@ -0,0 +1,11 @@
+package CreationalPatterns.AbstractFactory;
+
+public class RoundedSquare implements Shape {
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Inside RoundedSquare::draw() method.");
+	}
+
+}
---
+++ src/CreationalPatterns/AbstractFactory/Shape.java
@@ -0,0 +1,7 @@
+package CreationalPatterns.AbstractFactory;
+
+public interface Shape {
+	
+	public void draw();
+
+}
